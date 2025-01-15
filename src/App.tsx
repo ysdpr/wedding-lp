450bfbe857cd50c734cca18ae662b11f3d3df5de
ysdpr <sk.raiyu1488@gmail.com>
2025-01-16 00:46:23 +0900

    feat: 新しいスタイルと背景画像を追加し、レスポンシブデザインを改善

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,10 @@ function App() {
   const pi4 = useRef(null);
   const pi5 = useRef(null);
 
+
+  const textup1 = useRef(null);
+  const textup2 = useRef(null);
+
   useEffect(() => {
     const element = ref.current;
     const el2 = pi.current;
@@ -28,83 +32,101 @@ function App() {
     const el6 = pi5.current;
 
 
+    const pTextup1 = textup1.current;
+    const pTextup2 = textup2.current;
+
+
     gsap.to(element, {
-      // x: '40%', // 左に移動
       backgroundColor: '#dbd0e6',
       scrollTrigger: {
         trigger: element,
-        scrub: true, // スクロールに応じてアニメーションさせる
+        scrub: true,
       },
-    }); gsap.to(element2, {
-      // x: '40%', // 左に移動
+    });
+    
+    gsap.to(element2, {
       backgroundColor: '#f0908d',
       scrollTrigger: {
         trigger: element2,
         start: 'center 70%',
         end: 'center 30%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        scrub: true,
       },
     });
-    // [#2a4073]
-    // '#507ea4''
+
     gsap.to(el2, {
-      x: '0%', // 左に移動
-      // backgroundColor: '#FF0000',
+      x: '0%',
       scrollTrigger: {
         trigger: el2,
         start: 'center 96%',
         end: 'center 60%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        scrub: true,
       },
     });
+
     gsap.to(el3, {
-      x: '0%', // 左に移動
-      // backgroundColor: '#FF0000',
+      x: '0%',
       scrollTrigger: {
         trigger: el3,
         start: 'center 96%',
-        end: 'center 60%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        end: 'center 70%',
+        scrub: true,
       },
     });
 
     gsap.to(el4, {
-      filter: 'blur(0px)', // 左に移動
-      // backgroundColor: '#FF0000',
+      filter: 'blur(0px)',
       scrollTrigger: {
         trigger: el4,
         start: 'center 96%',
-        end: 'center 50%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        end: 'center 70%',
+        scrub: true,
       },
     });
 
-
-
-
     gsap.to(el5, {
-      opacity: '0', // 左に移動
-      // backgroundColor: '#FF0000',
+      opacity: '0',
       scrollTrigger: {
         trigger: el5,
         start: 'center 100%',
         end: 'center 50%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        scrub: true,
       },
     });
 
 
     gsap.to(el6, {
-      bottom: '10px', // 左に移動
+      bottom: '10px',
       color: '#393f4c',
-      // backgroundColor: '#FF0000',
       scrollTrigger: {
-        trigger: el5,
+        trigger: el6,
         start: 'center 75%',
         end: 'center 40%',
-        scrub: true, // スクロールに応じてアニメーションさせる
+        scrub: true,
       },
     });
+
+
+    gsap.to(pTextup1, {
+      y: '0%',
+      opacity : '1',
+      scrollTrigger: {
+        trigger: pTextup1,
+        start: 'center 94%',
+      },
+    });
+
+    gsap.to(pTextup2, {
+      y: '0%',
+      opacity : '1',
+      scrollTrigger: {
+        trigger: pTextup2,
+        start: 'center 94%',
+      },
+    });
+
+
+
   }, []);
 
 
@@ -136,9 +158,10 @@ function App() {
   useCountDownInterval(countTime, setCountTime)
   return (
     <>
-      <div className="scroll-bar"></div>
+     {/* TODO: ドロワーメニュー作成 */}
+      <div className="scroll-bar"></div> {/* TODO: スクロールバー作成 */}
       <div className="overflow-x-hidden">
-        <div className="w-[100vw] h-[100vh] py-8 relative">
+        <div className="w-[100vw] h-[100vh] py-8 relative"> {/* TODO: 案を出す */}
           <div className="w-full h-full absolute top-0 left-0 bg-z opacity-60"></div>
           {/* <div className="text-[#302833] text-4xl paint2 shuku bg-[#c85554] w-80 h-80 rounded-full grid place-items-center pb-6 absolute top-0 left-0 translate-x-[-50%] translate-y-[-50%]"></div> */}
           <div className="w-full h-full relative z-10 flex flex-col items-center justify-center">
@@ -160,7 +183,7 @@ function App() {
           </div>
         </div>
 
-        <div className="w-[100vw] h-[100vh] flex justify-center items-center snap-start relative">
+        <div className="w-[100vw] h-[88vh] flex justify-center items-center snap-start relative">
           <div className="w-full h-full absolute top-0 left-0 bg-f opacity-30"></div>
 
           <div className="relative z-10 flex flex-col text-l md:text-2xl md:leading-normal [writing-mode:vertical-rl]">
@@ -173,12 +196,10 @@ function App() {
               お慶び申し上げます
             </p>
             <p className="mr-3 md:mr-4">
-              このたび私たちは<br className="hidden md:inline" />
+              このたび私たちは<br className="hidden md:inline" /><span className="md:hidden">　</span>
               結婚式を挙げることになりました<br />
-              つきましては皆様により一層の<br className="hidden md:inline" />
-              ご指導を賜りたく<br />
-              ささやかではございますが披露宴を<br className="hidden md:inline" />
-              催したく存じます
+              つきましては　幾久しくご懇情を賜りたく<br />
+              披露かたがた小宴を催したく存じます
             </p>
             <p className="mr-3 md:mr-4">
               ご多用中誠に恐縮ではございますが<br />
@@ -198,33 +219,34 @@ function App() {
           </div>
         </div>
 
-        <div ref={ref2} className="w-[100vw] py-12 bg-[#2a4073] snap-start px-6">
-          <div className="flex justify-center">
-            <div ref={pi} className="relative w-full aspect-[3/2] translate-x-[150%]">
-              <img src={img1} className="imgclip absolute top-0 left-0" alt="" />
-              <p className="text-white text-2xl absolute bottom-0 right-0">
-                櫻井 友貴
-              </p>
+        <div ref={ref2} className="w-[100vw] bg-[#2a4073] snap-start px-4 md:py-0 bg-t1">
+          <div className="md:w-[720px] m-auto zzz px-8 md:px-10 py-12 md:py-24 text-[#444]">
+            <div className="t-width ml-[-3rem]">
+              <div ref={pi} className="flex gap-2 items-end justify-between w-full translate-x-[150%]">
+                <img src={img1} className="w-0 flex-grow imgclip" alt="" />
+                <p className="shrink-0 text-2xl md:text-3xl">
+                  櫻井 友貴
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="text-white text-md mt-6 leading-7">
-            <p>1991年8月29日、東京都生まれ。エンジニア。</p>
-            <p>プログラミング・DIY・洋菓子作りなど、ものづくりが好き。</p>
-            <p className="mt-3">美味しい料理・お酒をご用意してお待ちしております。</p>
-          </div>
-          <div className="flex justify-center mt-8">
-            <div ref={pi2} className="relative w-full aspect-[3/2] translate-x-[-150%]">
-              <img src={img2} className="imgclip2 absolute bottom-0 right-0" alt="" />
-              <p className="text-white text-2xl absolute top-0 left-0">
-                岡本 果波
-              </p>
+            <div ref={textup1} className="text-sm mt-6 translate-y-16 opacity-0 md:text-right md:text-base">
+              <p>1991年8月29日、東京都生まれ。エンジニア。</p>
+              <p>プログラミング・DIY・洋菓子作りなど、ものづくりが好き。</p>
+              <p className="mt-3">美味しい料理・お酒をご用意してお待ちしております。</p>
             </div>
-
-          </div>
-          <div className="text-white text-md mt-6 leading-7">
-            <p>1998年10月11日、兵庫県生まれ。ホテルスタッフ。</p>
-            <p>イラスト・デザインが得意で、</p>
-            <p className="mt-3">当日、みなさまにお会いできることを楽しみにしています。</p>
+            <div className="t-width mr-[-3rem] mt-8">
+              <div ref={pi2} className="flex flex-row-reverse gap-2 items-end justify-between w-full translate-x-[-150%]">
+                <img src={img2} className="w-0 flex-grow imgclip2" alt="" />
+                <p className="shrink-0 text-2xl md:text-3xl">
+                  岡本 果波
+                </p>
+              </div>
+            </div>
+            <div ref={textup2} className="text-sm mt-6 translate-y-16 opacity-0 md:text-base">
+              <p>1998年10月11日、兵庫県生まれ。ホテルスタッフ。</p>
+              <p>FIXME</p> {/* TODO: 文言入力 */}
+              <p className="mt-3">当日、みなさまにお会いできることを楽しみにしています。</p>
+            </div>
           </div>
         </div>
 
@@ -244,54 +266,61 @@ function App() {
           </div>
         </div>
 
-        <div ref={ref} className="w-[100vw] bg-[#c1e4e9] py-12 px-4">
-          <h2 className="text-3xl text-center">ご案内</h2>
-          <div className="">
-            <p className="text-md mt-2">日付</p>
-            <p className="flex items-end mt-1 pl-2">
-              <span className="text-4xl ovo">2025</span>
-              <span className="text-2xl ml-1 pb-1">年</span>
-              <span className="text-4xl ml-1 ovo">03</span>
-              <span className="text-2xl ml-1 pb-1">月</span>
-              <span className="text-4xl ml-1 ovo">21</span>
-              <span className="text-2xl ml-1 pb-1">日</span>
-              <span className="text-2xl ml-1 pb-1">(金)</span>
-            </p>
-            <dl className="flex flex-col">
-              <div className="flex items-end gap-4">
-                <div className="flex items-end gap-4 text-2xl pl-2">
-                  <dt className="mb-1 w-20">挙式</dt>
-                  <dd className="w-16 ovo">11:30</dd>
-                </div>
-                <div className="flex items-end gap-2 text-sm text-slate-600">
-                  <dt className="mb-1">受付</dt>
-                  <dd className="mb-0.5 w-16 ovo">10:30</dd>
-                </div>
-              </div>
-              <div className="flex items-end gap-4 pl-2">
-                <div className="flex items-end gap-4 text-2xl">
-                  <dt className="mb-1 w-20">披露宴</dt>
-                  <dd className="w-16 ovo">12:45</dd>
-                </div>
-                <div className="flex items-end gap-2 text-sm text-slate-600">
-                  <dt className="mb-1">受付</dt>
-                  <dd className="mb-0.5 w-16 ovo">12:15</dd>
-                </div>
-              </div>
-            </dl>
-          </div>
-          <h2 className="text-3xl text-center mt-8">アクセス</h2>
-          <p className="mt-2">会場</p>
-          <p className="text-3xl mt-1">神田明神</p>
-          <div className="pl-2">
-            <p className="mt-2 text-sm">〒101-0021 東京都千代田区外神田2-16-2</p>
-            <p className="text-sm">03-6384-0477</p>
-            <p className="text-sm">https://myojin.tokyo/</p>
-          </div>
-          <div>
-            <div className="mt-4 w-full h-60">
-              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.005369730445!2d139.76525902578862!3d35.70148547258068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c1ecc161bed%3A0xf397e4b6bf417c8b!2z56We55Sw56We56S-77yI56We55Sw5piO56We77yJ!5e0!3m2!1sja!2sjp!4v1736514079080!5m2!1sja!2sjp" className="w-full h-full" loading="lazy"></iframe>
+        <div ref={ref} className="w-[100vw] bg-[#c1e4e9] py-12 px-6 md:py-24 bg-ex"> {/* TODO: BGのテクスチャを変える */}
+          <div className="md:w-[720px] m-auto">
 
+            <div className="md:m-auto md:w-[320px]">
+              <h2 className="text-2xl text-center">ご案内</h2> {/* TODO: 見出しの装飾を作る */}
+              <div className="">
+                <p className="text-md mt-2">日程</p>
+                <p className="flex items-end mt-1">
+                  <span className="text-4xl ovo">2025</span>
+                  <span className="text-2xl ml-1 pb-1">年</span>
+                  <span className="text-4xl ml-1 ovo">03</span>
+                  <span className="text-2xl ml-1 pb-1">月</span>
+                  <span className="text-4xl ml-1 ovo">21</span>
+                  <span className="text-2xl ml-1 pb-1">日</span>
+                  <span className="text-2xl ml-1 pb-1">(金)</span>
+                </p>
+                <dl className="flex flex-col">
+                  <div className="flex items-end gap-4">
+                    <div className="flex items-end gap-4 text-2xl">
+                      <dt className="mb-1 w-20">挙式</dt>
+                      <dd className="w-16 ovo">11:30</dd>
+                    </div>
+                    <div className="flex items-end gap-2 text-sm text-slate-600">
+                      <dt className="mb-1">受付</dt>
+                      <dd className="mb-0.5 w-16 ovo">10:30</dd>
+                    </div>
+                  </div>
+                  <div className="flex items-end gap-4">
+                    <div className="flex items-end gap-4 text-2xl">
+                      <dt className="mb-1 w-20">披露宴</dt>
+                      <dd className="w-16 ovo">12:45</dd>
+                    </div>
+                    <div className="flex items-end gap-2 text-sm text-slate-600">
+                      <dt className="mb-1">受付</dt>
+                      <dd className="mb-0.5 w-16 ovo">12:15</dd>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+
+            <div className="md:m-auto md:w-[320px]">
+
+              <h2 className="text-2xl text-center mt-8">アクセス</h2>
+              <p className="mt-2">会場</p>
+              <p className="text-3xl mt-1">神田明神</p>
+              <div className="">
+                <p className="mt-2 text-sm">〒101-0021 東京都千代田区外神田2-16-2</p>
+                <p className="text-sm">03-6384-0477</p>
+                <p className="text-sm">https://myojin.tokyo/</p>
+              </div>
+            </div>
+
+            <div className="mt-4 md:mt-8 w-full h-60 md:h-[36rem]">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.005369730445!2d139.76525902578862!3d35.70148547258068!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c1ecc161bed%3A0xf397e4b6bf417c8b!2z56We55Sw56We56S-77yI56We55Sw5piO56We77yJ!5e0!3m2!1sja!2sjp!4v1736514079080!5m2!1sja!2sjp" className="w-full h-full" loading="lazy"></iframe>
             </div>
           </div>
 
